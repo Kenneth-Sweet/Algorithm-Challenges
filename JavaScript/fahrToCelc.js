2f5d56737f4fe677b89d8deabc797ca17d0b1a99
Kenneth Sweet <milandalosur@gmail.com>
2018-02-12 03:42:50 +0000

    Create fahrToCelc.js

---
+++ JavaScript/fahrToCelc.js
@@ -0,0 +1,17 @@
+/*This code is based upon an example in The C Programming Language that I have translated to JavaScript. This code could be improved
+by placing lower, upper, and step as parameters */
+
+function fahrToCelc() {
+  var lower = 0, upper = 300, step = 20;
+  
+  var fahr = lower
+    while (fahr <= upper) {
+    celsius = 5 * (fahr - 32) /9;
+    fahr += step;
+    console.log(fahr, celsius);
+    }
+ 
+  }
+  
+  function fahrToCelc()
+  
