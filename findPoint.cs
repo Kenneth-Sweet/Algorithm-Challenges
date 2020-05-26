5870276af78bfe91ea6c8f8fd98cf43878d99933
Kenneth Sweet <kenny.sweet@hotmail.com>
2020-05-26 10:20:45 -0400

    Delete findPoint.cs

--- findPoint.cs
+++
@@ -1,6 +0,0 @@
-  static int[] findPoint(int px, int py, int qx, int qy) {
-        int [] values = new int[2];
-        values[0] = qx-px + qx;
-        values[1] = qy-py + qy;
-        return values;
-    }
